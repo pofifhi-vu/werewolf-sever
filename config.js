a2530c54adbf5e91c0fe9474ec61454a745d09ab
pofifhi-vu <pofifhi@mail.dangminhhoa.edu.vn>
2025-09-06 10:39:26 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ZALO_APP_ID: "2554",
+  ZALO_KEY1: "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn",
+  ZALO_KEY2: "trMrHtvjo6myautxDUiAcYsVtaeQ8nhf",
+  ZALO_ENDPOINT: "https://sandbox.zalopay.com.vn/v001/tpe/createorder"
+}
